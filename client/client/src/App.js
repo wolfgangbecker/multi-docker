b809134f963ad8bca9784dae035c7dc6f10fdfea
Wolfgang Becker <wolfgang@cobalt.io>
2019-03-09 14:06:03 +0100

    Initial commit

---
+++ client/client/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
